04aeee7884fcdd84967dd274a82f17a6b0fbd0e3
Chirag Dahiya <122281803+CDahiya333@users.noreply.github.com>
2025-03-16 20:56:31 +0530

    Configured axios and proxy

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: import.meta.env.DEV
+//     ? "/api"
+//     : import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
+//   withCredentials: true,
+// });
+
+const axiosInstance = axios.create({
+  baseURL: "/api", // Always use relative path in development
+  withCredentials: true,
+});
+
+export default axiosInstance;
