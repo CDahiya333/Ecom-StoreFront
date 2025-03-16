425adac7f6edcd1fbc9190644244390ba8e9fa9a
Chirag Dahiya <122281803+CDahiya333@users.noreply.github.com>
2025-03-16 20:56:16 +0530

    Testing without Limiters

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,13 +5,18 @@ import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/signup",authRateLimiter, signup);
-router.post("/login",authRateLimiter, login);
+// router.post("/signup",authRateLimiter, signup);
+// router.post("/login",authRateLimiter, login);
+router.post("/signup", signup);
+router.post("/login", login);
 router.post("/logout", logout);
 router.post("/refresh-token", tokenRefresher);
-router.post("/sendotp", otpRateLimiter, sendOtp);
-router.post("/verifyotp", otpRateLimiter, verifyOtp);
-router.post("/resend-otp",  otpRateLimiter, resendOtp);
+// router.post("/sendotp", otpRateLimiter, sendOtp);
+// router.post("/verifyotp", otpRateLimiter, verifyOtp);
+// router.post("/resend-otp",  otpRateLimiter, resendOtp);
+router.post("/sendotp", sendOtp);
+router.post("/verifyotp", verifyOtp);
+router.post("/resend-otp", resendOtp);
 router.get("/getProfile",protectRoute, getProfile);
 
 export default router;
