596fee0a8ca3f45fda6365018547af0862f1365e
Chirag Dahiya <122281803+CDahiya333@users.noreply.github.com>
2025-04-16 23:58:08 +0530

    axios reconfig

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,13 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.DEV
-    ? "/api"
-    : import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
+  baseURL: import.meta.mode === "development" ? "http://localhost:3000" : "/api",
   withCredentials: true,
 });
 
 // const axiosInstance = axios.create({
 //   baseURL: "/api", // Always use relative path in development
-//   withCredentials: true,
+//   withCredentials: true, 
 // });
 
 export default axiosInstance;
